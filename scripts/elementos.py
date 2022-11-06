20b4dbc8e009b7bd0c3ad51d97ee6a3080153fce
german valle trujillo <gervalletrujillo@gmail.com>
2022-11-05 21:17:12 -0600

    Reorganizacion de carpetas

---
+++ scripts/elementos.py
@@ -0,0 +1,7 @@
+#%%
+
+import pandas as pd
+import yfinance as 
+
+
+
--- testing.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-
-r = np.linspace(0,100,10)
-
-for i,j in enumerate(r):
-    print(f"{i} -> {j}")
