913cb1d16810d3357028d7f539798f2619654d8e
GerValle <gervalletrujillo@gmail.com>
2023-07-22 02:35:38 +0000

    vamos a ver

--- scripts/elementos.py
+++ scripts/elementos.py
@@ -1,4 +1,35 @@
 #%%
 
-print("Hola")
+import yfinance as yf
 
+symbols = ['AAPL','F', 'C']
+
+tickers = yf.Tickers(symbols)
+
+dias = 500
+
+h = tickers.history(period=f"{dias}d")['Close']
+
+#%%
+rendimientos = h.pct_change().dropna()
+
+#%%
+
+import sqlite3 as lite
+
+from pathlib import Path
+
+mi_base = Path.cwd() / 'mi_base.sqlite'
+
+conn = lite.connect(mi_base)
+
+
+
+conn.close()
+
+
+
+
+
+
+# %%
